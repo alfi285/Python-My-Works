2a36089dfab0039e349fcea0bbffb10a4eec64ce
ALFIYA A <alfiyazon@gmail.com>
2024-10-24 22:16:24 +0530

    24-10-24

---
+++ 124 nestd dict.py
@@ -0,0 +1,23 @@
+
+
+dictmain ={
+    "dict1": {
+        "name" : "abc",
+        "age" : 45,
+        "place" : "clt"
+    },
+
+    "dict2": {
+        "name" : "cde",
+        "age" : 45,
+        "subject" : "cs"
+    },
+
+    "dict3": {
+        "name" : "efg",
+        "place" : "clt",
+        "core" : "cs"
+    }
+}
+
+print(dictmain)
